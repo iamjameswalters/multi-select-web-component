5cd259c5af9db3d40685325f6e7b379d592a16d4
James Walters <33947683+iamjameswalters@users.noreply.github.com>
2024-03-05 17:31:04 -0500

    Why does this.querySelectorAll('option') fail?

--- multiselect.js
+++ multiselect.js
@@ -1,18 +1,55 @@
 class MultiSelect extends HTMLElement {
-  // Make a ShadowDOM
-  shadowRoot = this.attachShadow({mode: "open"})
+  constructor() {
+    super()
 
-  connectedCallback() {
-    this.shadowRoot.innerHTML = "<p>Hello world!</p>"
+    const shadowRoot = this.attachShadow({mode: "open"})
+    
+    const unselected = document.createElement("ul")
+    const selected = document.createElement("ul")
+    
+    let options = []
+    for (option of this.querySelectorAll("option")) {
+      options.push(option.cloneNode(true))
+    }
+
+    console.log(options)
+    for (item of options) {
+      let li = document.createElement("li")
+      li.innerText = item.innerText
+
+      console.log(li)
+      if (item.selected) {
+        selected.appendChild(li)
+      } else {
+        unselected.appendChild(li)
+      }
+    }
+    let borg = document.createElement("li")
+    borg.innerText = "default"
+    selected.appendChild(borg)
+    const sel_label = document.createElement("h1")
+    sel_label.innerText = "Selected:"
+    const unsel_label = document.createElement("h1")
+    unsel_label.innerText = "Unselected:"
+
+    // shadowRoot.appendChild(sel_label)
+    shadowRoot.appendChild(selected)
+    // shadowRoot.appendChild(unsel_label)
+    shadowRoot.appendChild(unselected)
+
   }
 
-  // disconnectedCallback() {
+  // shadowRoot = this.attachShadow({mode: "open"})
 
-  // }
-
-  // attributeChangedCallback() {
-
-  // }
+  connectedCallback() {
+    let greeting = document.createElement("h1")
+    greeting.innerHTML = "Houston"
+    // let template = document.createElement("template")
+    // template.innerHTML = "<ul><slot></slot></ul>"
+    // let templateContent = template.content
+    // this.shadowRoot.appendChild(greeting)
+    // this.shadowRoot.appendChild(templateContent.cloneNode(true))
+  }
 }
 
 // Define element if not already defined

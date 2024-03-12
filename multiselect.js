97e877862efa5b67062ee2a7a951af006beca763
James Walters <33947683+iamjameswalters@users.noreply.github.com>
2024-03-12 12:24:49 -0400

    Move options back and forth between selects

--- multiselect.js
+++ multiselect.js
@@ -4,41 +4,66 @@ class MultiSelect extends HTMLElement {
 
     const shadowRoot = this.attachShadow({mode: "open"})
     
-    const unselected = document.createElement("select")
-    const selected = document.createElement("select")
+    this.unselected = document.createElement("select")
+    this.selected = document.createElement("select")
     
-    let options = []
+    this.options = []
     for (let option of this.querySelectorAll("option")) {
-      options.push(option.cloneNode(true))
+      this.options.push(option.cloneNode(true))
     }
 
-    for (let item of options) {
+    for (let item of this.options) {
       if (item.selected) {
         item.selected = false
-        selected.appendChild(item)
+        this.selected.appendChild(item)
       } else {
-        unselected.appendChild(item)
+        this.unselected.appendChild(item)
       }
     }
     
-    const sel_label = document.createElement("h1")
-    sel_label.innerText = "Selected:"
-    const unsel_label = document.createElement("h1")
-    unsel_label.innerText = "Unselected:"
+    this.sel_label = document.createElement("h1")
+    this.sel_label.innerText = "Selected:"
+    this.unsel_label = document.createElement("h1")
+    this.unsel_label.innerText = "Unselected:"
 
-    shadowRoot.appendChild(sel_label)
-    shadowRoot.appendChild(selected)
-    shadowRoot.appendChild(unsel_label)
-    shadowRoot.appendChild(unselected)
+    this.sel_btn = document.createElement("button")
+    this.sel_btn.innerText = "Select"
+    this.desel_btn = document.createElement("button")
+    this.desel_btn.innerText = "Deselect"
+    this.btn_div = document.createElement("div")
+    this.btn_div.appendChild(this.sel_btn)
+    this.btn_div.appendChild(this.desel_btn)
 
+    shadowRoot.appendChild(this.sel_label)
+    shadowRoot.appendChild(this.selected)
+    shadowRoot.appendChild(this.btn_div)
+    shadowRoot.appendChild(this.unsel_label)
+    shadowRoot.appendChild(this.unselected)
+
+  }
+
+  selBtnFunc = () => {
+    const el = this.unselected
+    const opt = el.options[el.selectedIndex]
+    el.removeChild(opt)
+    this.selected.appendChild(opt)
+  }
+
+  deselBtnFunc = () => {
+    const el = this.selected
+    const opt = el.options[el.selectedIndex]
+    el.removeChild(opt)
+    this.unselected.appendChild(opt)
   }
 
   connectedCallback() {
-    // Not sure what to put here yet
+    this.sel_btn.addEventListener("click", this.selBtnFunc)
+    this.desel_btn.addEventListener("click", this.deselBtnFunc)
   }
 
   disconnectedCallback() {
-    // Not sure what to put here yet
+    this.sel_btn.removeEventListener("click", this.selBtnFunc)
+    this.desel_btn.removeEventListener("click", this.deselBtnFunc)
   }
 }
 
